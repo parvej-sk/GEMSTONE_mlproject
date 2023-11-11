f46b35ee1297cda154a9741d41a0c372d5337228
Parvej Sheikh <106231462+parvej-sk@users.noreply.github.com>
2023-11-11 15:41:56 +0530

    Update training_pipeline.py

--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -20,6 +20,7 @@ if __name__=="__main__":
         train_arr,test_arr,_=data_transformation.initaite_data_transformation(train_data_path, test_data_path) 
 
         # Model Training
+        #Model Training
         model_trainer = ModelTrainer()
         print(model_trainer.initate_model_training(train_arr,test_arr))
         
